02a85ea1dac81c9296011205c041e093cdef2b8a
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-06-07 01:04:19 +0300

    Linters.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: [
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:vue/essential',
+    '@vue/prettier',
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: 'babel-eslint',
+  },
 };
