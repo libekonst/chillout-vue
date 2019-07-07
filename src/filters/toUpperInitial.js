f8004f8638058170ed8ff9516a0d249de3dfd766
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-07-07 06:33:19 +0300

    Update toUpperInitial.js

--- src/filters/toUpperInitial.js
+++ src/filters/toUpperInitial.js
@@ -1,14 +1,5 @@
-// import Vue from 'vue';
-
 export function toUpperInitial(value) {
   if (!value) return '';
-
-  return (
-    value
-      .toString()
-      .charAt(0)
-      .toUpperCase() + value.slice(1)
-  );
+  const val = value.toString();
+  return val.charAt(0).toUpperCase() + val.slice(1);
 }
-
-// Vue.filter('toUpperInitial', toUpperInitial);
