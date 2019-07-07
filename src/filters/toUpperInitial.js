7aa0a7ad90e14e0723468ed32cfce5de693eac07
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-07-07 06:32:23 +0300

    Update toUpperInitial.js

--- src/filters/toUpperInitial.js
+++ src/filters/toUpperInitial.js
@@ -2,8 +2,13 @@
 
 export function toUpperInitial(value) {
   if (!value) return '';
-  value = value.toString();
-  return value.charAt(0).toUpperCase() + value.slice(1);
+
+  return (
+    value
+      .toString()
+      .charAt(0)
+      .toUpperCase() + value.slice(1)
+  );
 }
 
 // Vue.filter('toUpperInitial', toUpperInitial);
